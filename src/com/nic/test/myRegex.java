58de0601e4c05e09f11bad6596b79eb7127ddbb5
Nick <nikbat@users.noreply.github.com>
2015-05-18 14:02:50 -0700

    random

---
+++ src/com/nic/test/myRegex.java
@@ -0,0 +1,12 @@
+package com.nic.test;
+
+import java.util.regex.Pattern;
+
+public class myRegex {
+	
+	//public Pattern pattern = Pattern.compile("^(?:\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])(?:\\.(?:\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])){3}$");
+	public String pattern = "^(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})$";
+	
+	
+
+}
